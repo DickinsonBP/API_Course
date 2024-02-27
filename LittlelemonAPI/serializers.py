89a8084197c07c8e4d188e8fd9c84305fa932b81
Dickinson Bedoya Perez <dickinsonbp@gmail.com>
2024-02-27 12:30:07 +0100

    finished

--- LittlelemonAPI/serializers.py
+++ LittlelemonAPI/serializers.py
@@ -11,23 +11,19 @@ class CategorySerializer(serializers.ModelSerializer):
         fields = ['id','slug','title']
         
 class MenuItemSerializer(serializers.ModelSerializer):
-    category = CategorySerializer(read_only=True)
-    category_id = serializers.IntegerField(write_only=True)
+    # category = CategorySerializer(read_only=True)
+    # category_id = serializers.IntegerField(write_only=True)
+    category = serializers.PrimaryKeyRelatedField(
+        queryset = Category.objects.all()
+    )
     class Meta:
         model = MenuItem
-        fields = ['id','title','price','featured','category','category_id']
+        fields = ['id','title','price','featured','category']
 
 class UserSerializer(serializers.ModelSerializer):
-    Date_Joined = serializers.SerializerMethodField()
-    date_joined = serializers.DateTimeField(write_only=True, default=datetime.now)
-    email = serializers.EmailField(required=False)
-
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'date_joined', 'Date_Joined']
-
-    def get_Date_Joined(self, obj):
-        return obj.date_joined.strftime('%Y-%m-%d')
+        fields = ['id','username','email']
 
 class CartSerializer(serializers.ModelSerializer):
     unit_price = serializers.DecimalField(max_digits=6, decimal_places=2, source='menuitem.price', read_only=True)
@@ -41,34 +37,16 @@ class CartSerializer(serializers.ModelSerializer):
 
 
 class OrderItemSerializer(serializers.ModelSerializer):
-    unit_price = serializers.DecimalField(max_digits=6, decimal_places=2, source='menuitem.price', read_only=True)
-    price = serializers.DecimalField(max_digits=6, decimal_places=2, read_only=True)
-    name = serializers.CharField(source='menuitem.title', read_only=True)
-
     class Meta:
         model = OrderItem
-        fields = ['name', 'quantity', 'unit_price', 'price']
-        extra_kwargs = {
-            'menuitem': {'read_only': True}
-        }
+        fields = ['order', 'menuitem', 'quantity', 'price']
 
 
 class OrderSerializer(serializers.ModelSerializer):
-    Date = serializers.SerializerMethodField()
-    date = serializers.DateTimeField(write_only=True, default=datetime.now)
-    # order_items = serializers.SerializerMethodField(method_name='get_order_items')
-    
+    orderitem = OrderItemSerializer(many=True, read_only=True, source='order')
+
     class Meta:
         model = Order
-        fields = ['id', 'user', 'delivery_crew', 'status', 'total', 'Date', 'date']
-        extra_kwargs = {
-            'total': {'read_only': True}
-        }
+        fields = ['id', 'user', 'delivery_crew',
+                  'status', 'date', 'total', 'orderitem']
 
-    def get_Date(self, obj):
-        return obj.date.strftime('%Y-%m-%d')
-
-    def get_order_items(self, obj):
-        order_items = OrderItem.objects.filter(order=obj)
-        serializer = OrderItemSerializer(order_items, many=True, context={'request': self.context['request']})
-        return serializer.data
